34b129166bb4a4aea6e4aaa68ade1f048dfc36eb
Slibzz <30203615+Slibzz@users.noreply.github.com>
2021-09-22 09:56:45 +0200

    Update Register.h

--- ARMSims/Register.h
+++ ARMSims/Register.h
@@ -1,5 +1,11 @@
 #pragma once
+#include <cstdint>
 class Register
 {
+private:
+	std::uint32_t Address;
+public:
+	Register(std::uint32_t Address);
+	std::uint32_t getAddress() { return Address; };
+	void setAddress(std::uint32_t newAddress);
 };
-
